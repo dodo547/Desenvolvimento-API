7fda9c3f5a7d229a6844d213c066ee526da99162
dodo547 <tapajoz.douglas@gmail.com>
2024-05-16 18:48:10 -0300

    Envio do Exercicio solicitado

---
+++ Exercicio-vendedor/src/main/java/br/com/serratec/entity/LancamentoVendas.java
@@ -0,0 +1,51 @@
+package br.com.serratec.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class LancamentoVendas {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigoVenda;
+	private LocalDate dataVenda;
+	private Double valorVenda;
+	private Vendedor vendedor;
+
+	public Long getCodigoVenda() {
+		return codigoVenda;
+	}
+
+	public void setCodigoVenda(Long codigoVenda) {
+		this.codigoVenda = codigoVenda;
+	}
+
+	public LocalDate getDataVenda() {
+		return dataVenda;
+	}
+
+	public void setDataVenda(LocalDate dataVenda) {
+		this.dataVenda = dataVenda;
+	}
+
+	public Double getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(Double valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+
+}
---
+++ Exercicio-vendedor/src/main/java/br/com/serratec/entity/Vendedor.java
@@ -0,0 +1,58 @@
+package br.com.serratec.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Vendedor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigoVendedor;
+	private String nome;
+	private String email;
+	private Double salario;
+	private Double comissao;
+	
+	public Long getCodigoVendedor() {
+		return codigoVendedor;
+	}
+
+	public void setCodigoVendedor(Long codigoVendedor) {
+		this.codigoVendedor = codigoVendedor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public Double getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(Double comissao) {
+		this.comissao = comissao;
+	}
+
+}

0d2cb1b1b0faaecf7684ce2cf2c7891e2a228c58
dodo547 <tapajoz.douglas@gmail.com>
2024-05-22 22:55:00 -0300

    Atualização Trabalho final

---
+++ Trabalho-final/src/main/java/com/serratec/br/service/EnderecoService.java
@@ -0,0 +1,42 @@
+package com.serratec.br.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.serratec.br.dto.EnderecoResponseDTO;
+import com.serratec.br.entity.Endereco;
+import com.serratec.br.exception.ResourceNotFoundException;
+import com.serratec.br.repository.EnderecoRepository;
+
+@Service
+public class EnderecoService {
+
+	@Autowired
+	private EnderecoRepository repository;
+
+	public EnderecoResponseDTO buscar(String cep) {
+		Optional<Endereco> endereco = Optional.ofNullable(repository.findByCep(cep));
+		if (endereco.isPresent()) {
+			return new EnderecoResponseDTO(endereco.get());
+
+		} else {
+			RestTemplate rs = new RestTemplate();
+			String url = "http://viacep.com.br/ws/" + cep + "/json";
+			Optional<Endereco> enderecoViaCep = Optional.ofNullable(rs.getForObject(url, Endereco.class));
+			if (enderecoViaCep.get().getCep() != null) {
+				String cepSemTraco = enderecoViaCep.get().getCep().replaceAll("-", "");
+				enderecoViaCep.get().setCep(cepSemTraco);
+				return inserir(enderecoViaCep.get());
+			} else {
+				throw new ResourceNotFoundException("Cep não localizado!");
+			}
+		}
+	}
+	public EnderecoResponseDTO inserir(Endereco endereco) {
+		return new EnderecoResponseDTO(repository.save(endereco));
+	}
+	
+}

399c3fd270edd53f34a5f5761f29ddf8e2613be6
administrador <erikromaneli@gmail.com>
2024-05-28 17:24:52 -0300

    finalzinho

--- Trabalho-final/src/main/java/com/serratec/br/controller/JogoController.java
+++ Trabalho-final/src/main/java/com/serratec/br/controller/JogoController.java
@@ -3,22 +3,46 @@ package com.serratec.br.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.serratec.br.dto.JogoResponseDTO;
+import com.serratec.br.entity.Jogo;
 import com.serratec.br.service.JogoService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RequestMapping("/jogos")
 @RestController
 public class JogoController {
-	@Autowired
-	private JogoService jogoService;
-	@GetMapping
-	public ResponseEntity<List<JogoResponseDTO>> listarJogos(){
-		return ResponseEntity.ok(jogoService.listarJogos());
-	}
+    @Autowired
+    private JogoService jogoService;
 
+    @GetMapping
+    public ResponseEntity<List<JogoResponseDTO>> listarJogos(){
+        return ResponseEntity.ok(jogoService.listarJogos());
+    }
+
+    @PostMapping
+    public ResponseEntity<JogoResponseDTO> inserirJogo(@RequestBody Jogo jogo){
+        return ResponseEntity.ok(jogoService.inserirJogo(jogo));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<JogoResponseDTO> atualizarJogo(@PathVariable Long id, @RequestBody Jogo jogo) throws NotFoundException{
+        return ResponseEntity.ok(jogoService.atualizarJogo(id, jogo));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarJogo(@PathVariable Long id) throws NotFoundException{
+        jogoService.deletarJogo(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- Trabalho-final/src/main/java/com/serratec/br/service/JogoService.java
+++ Trabalho-final/src/main/java/com/serratec/br/service/JogoService.java
@@ -2,8 +2,10 @@ package com.serratec.br.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.serratec.br.dto.JogoResponseDTO;
@@ -12,24 +14,46 @@ import com.serratec.br.repository.JogoRepository;
 
 @Service
 public class JogoService {
-	@Autowired
-	private JogoRepository jogoRepository;
-	public List<JogoResponseDTO> listarJogos() {
-		List <Jogo> jogos = jogoRepository.findAll();
-		List <JogoResponseDTO> jogosResponseDTO = new ArrayList <>();
-		for (Jogo jogo: jogos) {
-			
-			JogoResponseDTO jogoDTO = new JogoResponseDTO(jogo);
-			jogosResponseDTO.add(jogoDTO);
-		}
-		return jogosResponseDTO;
-		
-		
-	}
-	public JogoResponseDTO inserirJogo(Jogo jogo) {
-		Optional<Jogo> jogoOpt =
-	}
-	
-	
+    @Autowired
+    private JogoRepository jogoRepository;
+    public List<JogoResponseDTO> listarJogos() {
+        List <Jogo> jogos = jogoRepository.findAll();
+        List <JogoResponseDTO> jogosResponseDTO = new ArrayList <>();
+        for (Jogo jogo: jogos) {
+
+            JogoResponseDTO jogoDTO = new JogoResponseDTO(jogo);
+            jogosResponseDTO.add(jogoDTO);
+        }
+        return jogosResponseDTO;
+
+
+    }
+    public JogoResponseDTO inserirJogo(Jogo novoJogo) {
+
+        return new JogoResponseDTO(jogoRepository.save(novoJogo));
+    }
+
+    public JogoResponseDTO atualizarJogo(Long id, Jogo jogo) throws NotFoundException{
+
+        Optional<Jogo> jogoOpt = jogoRepository.findById(id);
+
+        if(jogoOpt.isEmpty()) {
+            throw new NotFoundException();
+        }
+
+        jogo.setId(id);
+        return new JogoResponseDTO( jogoRepository.save(jogo) );
+
+    }
+
+    public void deletarJogo(Long id) throws NotFoundException {
+        Optional<Jogo> jogoOpt = jogoRepository.findById(id);
+
+        if(jogoOpt.isEmpty()) {
+            throw new NotFoundException();
+        }
+
+        jogoRepository.deleteById(id);
+    }
 
 }
